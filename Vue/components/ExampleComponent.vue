6da160debe6cd137d7695ab8aab7890b32413142
planyway <92812919+planyway@users.noreply.github.com>
2023-01-27 19:20:52 +0800

    Add files via upload

---
+++ Vue/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">An Example Component</div>
+            <div class="card-body">I am an example component.</div>
+        </div>
+    </div>
+
+
+</template>
+<script>
+export default {
+    mounted() {
+        console.log("Example component mounted.");
+    }
+};
+</script>
---
+++ Vue/components/SubFolderNameDoesNotMatter/SubFolderComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">A Sub Folder Component</div>
+            <div class="card-body">Hi! I am a sample component that lives inside a subfolder.</div>
+        </div>
+    </div>
+</template>
