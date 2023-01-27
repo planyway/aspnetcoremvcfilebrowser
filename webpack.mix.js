d94d7575ef11a82a066b212d7decd2c21896885f
planyway <92812919+planyway@users.noreply.github.com>
2023-01-27 19:11:48 +0800

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,28 @@
+let mix = require('laravel-mix')
+const path = require('path')
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ | API Documentation: https://laravel-mix.com/docs
+ */
+
+mix.js('Vue/VUEsite.js', 'wwwroot/js/VUEsite.js').vue() // Compile our Vue entry point
+.styles('Vue/css', 'wwwroot/css/site.css') // Compile all files to app.css
+
+/*
+mix.js('Vue/VUEsite.js', 'wwwroot/site.js') // Compile our Vue entry point
+.styles('Vue/css', 'wwwroot/css/site.css') // Compile all files to app.css
+*/
+mix.webpackConfig({
+    resolve: {
+      alias: {
+        "@" : path.resolve(__dirname, 'Vue/components')
+      }
+    }
+  })
